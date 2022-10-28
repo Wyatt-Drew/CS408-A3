c2c5b0e183e25ff326eaacec16ed478ca33270d2
Wyatt-Drew <98035850+Wyatt-Drew@users.noreply.github.com>
2022-10-28 00:58:08 -0600

    WebGL and Windows Builds
    Added WebGL and Windows Builds

---
+++ A3-CS408/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,113 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CreateSpline::Start()
+extern void CreateSpline_Start_m58EAF917214883F0046B63FD972E451A2C99EC05 (void);
+// 0x00000002 System.Void CreateSpline::spawnControlPoints()
+extern void CreateSpline_spawnControlPoints_mC490E6267A1E2F42CB7D0EA4A18D873891025461 (void);
+// 0x00000003 System.Void CreateSpline::recordPosition()
+extern void CreateSpline_recordPosition_mBE7D01B91B3F9823F1E357AD6B7C90B36852AE71 (void);
+// 0x00000004 System.Void CreateSpline::calculateSpline()
+extern void CreateSpline_calculateSpline_m5715E8D245D1BA33AF0753F8EDC2699DFA85E3D0 (void);
+// 0x00000005 System.Single CreateSpline::B0(System.Single)
+extern void CreateSpline_B0_mE0F2E87FB97465631222563CFC4D64F4A9F2E670 (void);
+// 0x00000006 System.Single CreateSpline::B1(System.Single)
+extern void CreateSpline_B1_mF58AA2B8D74F6697271AD8289FD44C60840D5F93 (void);
+// 0x00000007 System.Single CreateSpline::B2(System.Single)
+extern void CreateSpline_B2_m9B1678A9D6EF211001BB69C8E94BACC8964D1291 (void);
+// 0x00000008 System.Single CreateSpline::B3(System.Single)
+extern void CreateSpline_B3_mE028C5C5C707D4FEFD514B5B6B947ED6B196EB00 (void);
+// 0x00000009 System.Void CreateSpline::Update()
+extern void CreateSpline_Update_mDF3BCCAFD8C1EA1FD6B58B4C7EE9E7D5A94756E1 (void);
+// 0x0000000A System.Void CreateSpline::getInput()
+extern void CreateSpline_getInput_m81F3BC1EF7795947B2B03FE0E5F9DC8450CFFDB7 (void);
+// 0x0000000B System.Int32 CreateSpline::getFrame()
+extern void CreateSpline_getFrame_m532DDD28CA049420EA7707A5273866731599CC7B (void);
+// 0x0000000C System.Void CreateSpline::move()
+extern void CreateSpline_move_mEE6BC457E43E78187EC076A602BEB3A2410E5482 (void);
+// 0x0000000D System.Void CreateSpline::linearProgress()
+extern void CreateSpline_linearProgress_mD346122106619058C6DCC87D840603123A730F46 (void);
+// 0x0000000E System.Void CreateSpline::sineProgress()
+extern void CreateSpline_sineProgress_m4F5814D697B4187FB43E26B46B13C9E9F1FE6B5D (void);
+// 0x0000000F System.Void CreateSpline::parabolicProgress()
+extern void CreateSpline_parabolicProgress_mA16B4CF0628B584798F3FE8D9E074ECDD444122B (void);
+// 0x00000010 System.Void CreateSpline::.ctor()
+extern void CreateSpline__ctor_m127DE90F8C6A9026DE811D92E9EA7DFDB5529800 (void);
+// 0x00000011 System.Void lineController::drawLine(UnityEngine.Transform[])
+extern void lineController_drawLine_mFF691C0F1DA2325F4DD35E692F03364CAA643AE4 (void);
+// 0x00000012 System.Void lineController::.ctor()
+extern void lineController__ctor_mAA2D899BE40C1FE4F9EB1BC6C4F0AF3D78ACF35F (void);
+static Il2CppMethodPointer s_methodPointers[18] = 
+{
+	CreateSpline_Start_m58EAF917214883F0046B63FD972E451A2C99EC05,
+	CreateSpline_spawnControlPoints_mC490E6267A1E2F42CB7D0EA4A18D873891025461,
+	CreateSpline_recordPosition_mBE7D01B91B3F9823F1E357AD6B7C90B36852AE71,
+	CreateSpline_calculateSpline_m5715E8D245D1BA33AF0753F8EDC2699DFA85E3D0,
+	CreateSpline_B0_mE0F2E87FB97465631222563CFC4D64F4A9F2E670,
+	CreateSpline_B1_mF58AA2B8D74F6697271AD8289FD44C60840D5F93,
+	CreateSpline_B2_m9B1678A9D6EF211001BB69C8E94BACC8964D1291,
+	CreateSpline_B3_mE028C5C5C707D4FEFD514B5B6B947ED6B196EB00,
+	CreateSpline_Update_mDF3BCCAFD8C1EA1FD6B58B4C7EE9E7D5A94756E1,
+	CreateSpline_getInput_m81F3BC1EF7795947B2B03FE0E5F9DC8450CFFDB7,
+	CreateSpline_getFrame_m532DDD28CA049420EA7707A5273866731599CC7B,
+	CreateSpline_move_mEE6BC457E43E78187EC076A602BEB3A2410E5482,
+	CreateSpline_linearProgress_mD346122106619058C6DCC87D840603123A730F46,
+	CreateSpline_sineProgress_m4F5814D697B4187FB43E26B46B13C9E9F1FE6B5D,
+	CreateSpline_parabolicProgress_mA16B4CF0628B584798F3FE8D9E074ECDD444122B,
+	CreateSpline__ctor_m127DE90F8C6A9026DE811D92E9EA7DFDB5529800,
+	lineController_drawLine_mFF691C0F1DA2325F4DD35E692F03364CAA643AE4,
+	lineController__ctor_mAA2D899BE40C1FE4F9EB1BC6C4F0AF3D78ACF35F,
+};
+static const int32_t s_InvokerIndices[18] = 
+{
+	3418,
+	3418,
+	3418,
+	3418,
+	2498,
+	2498,
+	2498,
+	2498,
+	3418,
+	3418,
+	3306,
+	3418,
+	3418,
+	3418,
+	3418,
+	3418,
+	2784,
+	3418,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	18,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
